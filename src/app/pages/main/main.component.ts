944c6db52a2806041a099c8b40b99d73ae6a28ae
Darsruiz <darsruiz@gmail.com>
2021-04-15 03:46:34 +0200

    TO DO, cambios esteticos en el main

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -9,6 +9,17 @@ export class MainComponent implements OnInit {
 
   constructor() { }
 
+  skinscampeones() {
+
+    let nombres = ['Ezreal','Vayne','Ashe'];
+    let skin = [1,2,3];
+
+    //https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Samira_0.jpg
+    //console.log('https://ddragon.leagueoflegends.com/cdn/img/champion/splash/'+nombres[1]+'_'+skin[1]+'.jpg');
+    console.log(`https://ddragon.leagueoflegends.com/cdn/img/champion/splash/${nombres[1]}_${skin[1]}.jpg`);
+  }
+
+
   ngOnInit(): void {
   }
 
