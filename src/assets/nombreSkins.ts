782a79f59d311cc3d41a76c73e4218a3be53e5ad
Xelac <68864369+Xelacnnister@users.noreply.github.com>
2021-04-18 13:57:08 +0200

    Actu. lista campeones TOP y JUNGLA

--- src/assets/nombreSkins.ts
+++ src/assets/nombreSkins.ts
@@ -2,8 +2,8 @@ export class nombreSkins {
 
   public static nombres = ['Kindred', 'Swain', 'Varus', 'Ziggs','Ezreal','Vayne','Ashe','Corki','Samira','Caitlyn','Twitch','Vayne','Ashe','Ezreal','Vayne','Ashe'];
 
-  public static nombresTop = ['Garen','Darius'];
-  public static nombresJungle = ['Kindred'];
+  public static nombresTop = ["Aatrox", "Akali", "Camille", "Cassiopea", "Cho-Gath", "Darius", "DrMundo", "Fiora", "Gankplank", "Garen", "Gnar", "Gwen", "Hecarim", "Heimerdinger", "Illaoi", "Irelia", "Jax", "Jayce", "Karma", "Kayle", "Kennen", "Kled", "Lillia", "Lucian", "Malphite", "Maokai", "Mordekaiser", "Nasus", "Nocturne", "Olaf", "Ornn", "Pantheon", "Poppy", "Quinn", "Renekton", "Rengar", "Riven", "Rumble", "Ryze", "Sett", "Shen", "Singed", "Sion", "Sylas", "Teemo", "Tryndamere", "Urgot", "Vayne", "Vladimir", "Volibear", "Wukong", "Yasuo", "Yorick", "Zac"];
+  public static nombresJungle = ["Amumu", "DrMundo", "Ekko", "Elise", "Evelynn", "Fiddlesticks", "Gragas", "Graves", "Hecarim", "Ivern", "Jax", "Karthus", "Kayn", "Kha-Zix", "Kindred", "Kled", "LeeSin", "Lillia", "MasterYi", "Nidalee", "Nocturne", "Nunu", "Olaf", "Poppy", "Rammus", "Rek-Sai", "Rengar", "Sejuani", "Sett", "Shaco", "Shyvana", "Skarner", "Sylas", "Taliyah", "Trundle", "Udyr", "Vi", "Viego", "Volibear", "Warwick", "Wukong", "XinZhao", "Zac"];
   public static nombresMid = ['Mid'];
   public static nombresAdc = ['Adc'];
   public static nombresSupport = ['Support'];
