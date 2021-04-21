d82d56890612f7991022bb1977cc9f879c217db5
Darsruiz <darsruiz@gmail.com>
2021-04-21 23:47:00 +0200

    NombreSkins Finalizado

--- public/assets/nombreSkins.ts
+++ public/assets/nombreSkins.ts
@@ -1,12 +1,45 @@
 export class nombreSkins {
 
-  public static nombres = ['Kindred', 'Swain', 'Varus', 'Ziggs','Ezreal','Vayne','Ashe','Corki','Samira','Caitlyn','Twitch','Vayne','Ashe','Ezreal','Vayne','Ashe'];
+  public static nombres = ["Aatrox", "Akali", "Camille", "Cassiopeia", "Chogath", "Darius", "DrMundo",
+  "Fiora", "Gangplank", "Garen", "Gnar","Gragas", "Gwen", "Hecarim", "Heimerdinger", "Illaoi",
+  "Irelia", "Jax", "Jayce", "Kalista", "Karma", "Kayle", "Kennen", "Kled", "Lillia", "Lucian",
+  "Malphite", "Maokai", "Mordekaiser", "Nasus", "Nocturne", "Olaf", "Ornn", "Pantheon", "Poppy",
+  "Quinn", "Renekton", "Rengar", "Riven", "Rumble", "Ryze","Sejuani", "Sett", "Shen", "Shyvana",
+  "Singed", "Sion", "Sylas", "TahmKench", "Teemo", "Trundle", "Tryndamere", "Urgot", "Viego",
+  "Vayne", "Vladimir", "Volibear", "Warwick", "MonkeyKing", "Yasuo", "Yone", "Yorick", "Zac"];
 
-  public static Top = ["Aatrox", "Akali", "Camille", "Cassiopeia", "Cho-Gath", "Darius", "DrMundo", "Fiora", "Gankplank", "Garen", "Gnar", "Gwen", "Hecarim", "Heimerdinger", "Illaoi", "Irelia", "Jax", "Jayce", "Karma", "Kayle", "Kennen", "Kled", "Lillia", "Lucian", "Malphite", "Maokai", "Mordekaiser", "Nasus", "Nocturne", "Olaf", "Ornn", "Pantheon", "Poppy", "Quinn", "Renekton", "Rengar", "Riven", "Rumble", "Ryze", "Sett", "Shen", "Singed", "Sion", "Sylas", "Teemo", "Tryndamere", "Urgot", "Vayne", "Vladimir", "Volibear", "Wukong", "Yasuo", "Yorick", "Zac"];
-  public static Jungle = ["Amumu", "DrMundo", "Ekko", "Elise", "Evelynn", "Fiddlesticks", "Gragas", "Graves", "Hecarim", "Ivern", "Jax", "Karthus", "Kayn", "Kha-Zix", "Kindred", "Kled", "LeeSin", "Lillia", "MasterYi", "Nidalee", "Nocturne", "Nunu", "Olaf", "Poppy", "Rammus", "Rek-Sai", "Rengar", "Sejuani", "Sett", "Shaco", "Shyvana", "Skarner", "Sylas", "Taliyah", "Trundle", "Udyr", "Vi", "Viego", "Volibear", "Warwick", "Wukong", "XinZhao", "Zac"];
-  public static Mid = ['Mid'];
-  public static Adc = ['Adc'];
-  public static Support = ['Support'];
+  public static Top = ["Aatrox", "Akali", "Camille", "Cassiopeia", "Chogath", "Darius", "DrMundo",
+  "Fiora", "Gangplank", "Garen", "Gnar","Gragas", "Gwen", "Hecarim", "Heimerdinger", "Illaoi",
+  "Irelia", "Jax", "Jayce", "Kalista", "Karma", "Kayle", "Kennen", "Kled", "Lillia", "Lucian",
+  "Malphite", "Maokai", "Mordekaiser", "Nasus", "Nocturne", "Olaf", "Ornn", "Pantheon", "Poppy",
+  "Quinn", "Renekton", "Rengar", "Riven", "Rumble", "Ryze","Sejuani", "Sett", "Shen", "Shyvana",
+  "Singed", "Sion", "Sylas", "TahmKench", "Teemo", "Trundle", "Tryndamere", "Urgot", "Viego",
+  "Vayne", "Vladimir", "Volibear", "Warwick", "MonkeyKing", "Yasuo", "Yone", "Yorick", "Zac",];
+
+  public static Jungle = ["Amumu", "Darius", "Diana", "DrMundo", "Ekko", "Elise", "Evelynn",
+  "Fiddlesticks", "Gragas", "Graves", "Gwen", "Hecarim", "Ivern", "Jax", "Karthus", "Kayn",
+  "Khazix", "Kindred", "Kled", "LeeSin", "Lillia", "MasterYi", "Nidalee", "Mordekaiser",
+  "Morgana", "Nidalee", "Nocturne", "Nunu", "Olaf", "Poppy", "Rammus", "RekSai", "Rengar",
+   "Rumble", "Sejuani","Sett", "Shaco", "Shyvana", "Skarner", "Sylas", "Taliyah", "Trundle",
+    "Udyr", "Vi", "Viego", "Volibear", "Warwick", "MonkeyKing", "XinZhao", "Zac"];
+
+  public static Mid = ["Ahri", "Akali","Anivia","Annie","AurelionSol","Azir","Brand",
+  "Cassiopeia","Chogath","Corki","Diana","Ekko","Fizz","Galio","Gangplank","Gwen",
+  "Heimerdinger","Irelia","Jayce","Kassadin","Katarina","Kayle","Kennen","Kled",
+  "Leblanc","Lissandra", "Lucian", "Lux", "Malphite", "Malzahar", "Neeko", "Nocturne",
+   "Orianna", "Pantheon", "Qiyana", "Renekton","Rumble","Ryze","Swain","Sylas","Syndra",
+  "Seraphine","Talon","Tryndamere","TwistedFate","Veigar","Velkoz","Viego","Viktor","Vladimir",
+"Xerath","Yasuo","Yone","Zed","Ziggs","Zilean", "Zoe"];
+
+  public static Adc = ["Aphelios","Ashe","Caitlyn","Draven","Ezreal","Heimerdinger",
+  "Jhin","Jinx","Kaisa","Kalista","KogMaw","Lucian","MissFortune","Samira","Senna",
+  "Sivir","Swain","TahmKench","Tristana","Twitch","Varus","Vayne","Veigar","Xayah","Ziggs"];
+
+  public static Support = ["Alistar","Bard","Blitzcrank","Brand","Braum","Galio","Gragas",
+  "Heimerdinger","Janna","Karma","Leona","Lulu","Lux","Maokai","Morgana","Nami","Nautilus",
+"Neeko","Pantheon","Pyke","Rakan","Rell","Senna","Sett","Shaco","Sona","Soraka","Swain","Seraphine",
+"TahmKench","Taliyah","Taric","Thresh","Veigar","Velkoz","Xerath","Yuumi","Zilean","Zyra"];
+
   public static Lineas = ['Top', 'Jungle', 'Mid', 'Adc', 'Support','nombres'];
 
   public static skin = [1,0,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
